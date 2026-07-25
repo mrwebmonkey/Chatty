4d8ef08d62cc06cb1441935b0e4453b9237adae2
mrwebmonkey <workercode03@gmail.com>
2026-07-26 03:26:02 +0530

    change the backend localhost with the render url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api',
-  withCredentials: true, // Include credentials in requests
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
